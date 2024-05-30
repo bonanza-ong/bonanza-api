9940945e0d7c560f31d7725eca1e125d6f7949b7
jhonatapers <jhonatapers@gmail.com>
2024-05-30 16:20:25 -0300

    [feat] adiciona usp da exception para DemandaItem inválido -> "DemandaItemInvalidoException"

--- src/main/java/ong/bonanza/beneficiarioapi/domain/service/DemandaItemService.java
+++ src/main/java/ong/bonanza/beneficiarioapi/domain/service/DemandaItemService.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import ong.bonanza.beneficiarioapi.domain.entity.Beneficiario;
 import ong.bonanza.beneficiarioapi.domain.entity.DemandaItem;
 import ong.bonanza.beneficiarioapi.domain.enumeration.StatusDemandaItem;
+import ong.bonanza.beneficiarioapi.domain.exception.DemandaItemInvalidoException;
 import ong.bonanza.beneficiarioapi.domain.exception.DemandaItemNaoEncontradaException;
+import ong.bonanza.beneficiarioapi.domain.exception.RecursoInvalidoException.Atributo;
 import ong.bonanza.beneficiarioapi.domain.repository.DemandaItemRepository;
 
 @RequiredArgsConstructor
@@ -25,6 +27,11 @@ public class DemandaItemService {
     }
 
     public DemandaItem cadastrar(DemandaItem demandaItem) {
+
+        if (demandaItem.getQuantidadeSolicitada() < 1)
+            throw new DemandaItemInvalidoException(
+                    new Atributo("quantidadeSolicitada", demandaItem.getQuantidadeSolicitada().toString(), "Deve ser maior que zero"));
+
         demandaItem.setStatus(StatusDemandaItem.AGUARDANDO_CONFIRMACAO);
         demandaItem.setQuantidadeAtendida(0);
         return demandaItemRepository.save(demandaItem);
