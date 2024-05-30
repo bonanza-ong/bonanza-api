b01d855c4ce0995e7212a8c0cfba0653b93a7053
jhonatapers <jhonatapers@gmail.com>
2024-05-30 16:51:43 -0300

    [chore] Muda indentação para utilizar 4 espaços

--- src/main/java/ong/bonanza/beneficiarioapi/application/usecase/IniciarAtendimentoDemandaItemUC.java
+++ src/main/java/ong/bonanza/beneficiarioapi/application/usecase/IniciarAtendimentoDemandaItemUC.java
@@ -21,76 +21,76 @@ import ong.bonanza.beneficiarioapi.domain.service.ProvedorService;
 @Component
 public class IniciarAtendimentoDemandaItemUC {
 
-	private final InciarAtendimentoDemandaItemUCMapper mapper;
+    private final InciarAtendimentoDemandaItemUCMapper mapper;
 
-	private final AtendimentoDemandaItemService atendimentoDemandaItemService;
+    private final AtendimentoDemandaItemService atendimentoDemandaItemService;
 
-	private final PessoaService pessoaService;
+    private final PessoaService pessoaService;
 
-	private final ProvedorService provedorService;
+    private final ProvedorService provedorService;
 
-	private final DemandaItemService demandaItemService;
+    private final DemandaItemService demandaItemService;
 
-	private final BeneficiarioService beneficiarioService;
+    private final BeneficiarioService beneficiarioService;
 
-	public AtendimentoDemandaItemDTO executar(NovoAtendimentoDemandaItem novoAtendimento) {
-		return mapper.toAtendimentoDemandaItemDTO(
-				novoAtendimento.quantidadeAtendimento,
-				atendimentoDemandaItemService.inciar(mapper.toAtendimentoDemandaItem(
-						novoAtendimento.quantidadeAtendimento,
-						demandaItemService.buscarPorIdEBeneficiario(
-								novoAtendimento.demandaItemId,
-								beneficiarioService.buscarPorId(novoAtendimento.beneficiarioId)),
-						provedorService
-								.buscarPorPessoa(pessoaService.buscarPorId(novoAtendimento.pessoaProvedoraId)))));
-	}
+    public AtendimentoDemandaItemDTO executar(NovoAtendimentoDemandaItem novoAtendimento) {
+        return mapper.toAtendimentoDemandaItemDTO(
+                novoAtendimento.quantidadeAtendimento,
+                atendimentoDemandaItemService.inciar(mapper.toAtendimentoDemandaItem(
+                        novoAtendimento.quantidadeAtendimento,
+                        demandaItemService.buscarPorIdEBeneficiario(
+                                novoAtendimento.demandaItemId,
+                                beneficiarioService.buscarPorId(novoAtendimento.beneficiarioId)),
+                        provedorService
+                                .buscarPorPessoa(pessoaService.buscarPorId(novoAtendimento.pessoaProvedoraId)))));
+    }
 
-	public record NovoAtendimentoDemandaItem(
-			UUID pessoaProvedoraId,
-			UUID beneficiarioId,
-			UUID demandaItemId,
-			Integer quantidadeAtendimento) {
-	}
+    public record NovoAtendimentoDemandaItem(
+            UUID pessoaProvedoraId,
+            UUID beneficiarioId,
+            UUID demandaItemId,
+            Integer quantidadeAtendimento) {
+    }
 
-	public record AtendimentoDemandaItemDTO(
-			UUID id,
-			StatusAtendimentoDemandaItem status,
-			Integer quantidadeProvida,
-			DemandaItemDTO demanda) {
-	}
+    public record AtendimentoDemandaItemDTO(
+            UUID id,
+            StatusAtendimentoDemandaItem status,
+            Integer quantidadeProvida,
+            DemandaItemDTO demanda) {
+    }
 
-	public record DemandaItemDTO(
-			UUID id,
-			UUID beneficiarioId,
-			Integer quantidadeSolicitada,
-			Integer quantidadeAtendida,
-			ItemDTO item) {
-	}
+    public record DemandaItemDTO(
+            UUID id,
+            UUID beneficiarioId,
+            Integer quantidadeSolicitada,
+            Integer quantidadeAtendida,
+            ItemDTO item) {
+    }
 
-	public record ItemDTO(
-			UUID id,
-			String nome) {
-	}
+    public record ItemDTO(
+            UUID id,
+            String nome) {
+    }
 
-	@Mapper
-	public interface InciarAtendimentoDemandaItemUCMapper {
+    @Mapper
+    public interface InciarAtendimentoDemandaItemUCMapper {
 
-		@Mapping(target = "demanda", source = "demandaItem")
-		@Mapping(target = "quantidade", source = "quantidadeAtendimento")
-		@Mapping(target = "status", ignore = true)
-		@Mapping(target = "id", ignore = true)
-		@Mapping(target = "updatedAt", ignore = true)
-		@Mapping(target = "createdAt", ignore = true)
-		AtendimentoDemandaItem toAtendimentoDemandaItem(
-				Integer quantidadeAtendimento,
-				DemandaItem demandaItem,
-				Provedor provedor);
+        @Mapping(target = "demanda", source = "demandaItem")
+        @Mapping(target = "quantidade", source = "quantidadeAtendimento")
+        @Mapping(target = "status", ignore = true)
+        @Mapping(target = "id", ignore = true)
+        @Mapping(target = "updatedAt", ignore = true)
+        @Mapping(target = "createdAt", ignore = true)
+        AtendimentoDemandaItem toAtendimentoDemandaItem(
+                Integer quantidadeAtendimento,
+                DemandaItem demandaItem,
+                Provedor provedor);
 
-		@Mapping(target = "demanda.beneficiarioId", source = "atendimentoDemanda.demanda.beneficiario.id")
-		AtendimentoDemandaItemDTO toAtendimentoDemandaItemDTO(
-				Integer quantidadeProvida,
-				AtendimentoDemandaItem atendimentoDemanda);
+        @Mapping(target = "demanda.beneficiarioId", source = "atendimentoDemanda.demanda.beneficiario.id")
+        AtendimentoDemandaItemDTO toAtendimentoDemandaItemDTO(
+                Integer quantidadeProvida,
+                AtendimentoDemandaItem atendimentoDemanda);
 
-	}
+    }
 
 }
