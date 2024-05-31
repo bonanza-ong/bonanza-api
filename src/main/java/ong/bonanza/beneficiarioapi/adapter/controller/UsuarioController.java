347bb24fdbe509d5116dafdd6267bed4a4a67d09
jhonatapers <jhonatapers@gmail.com>
2024-05-31 11:42:45 -0300

    [feat] autocadastramento de usuario

--- src/main/java/ong/bonanza/beneficiarioapi/adapter/controller/UsuarioController.java
+++ src/main/java/ong/bonanza/beneficiarioapi/adapter/controller/UsuarioController.java
@@ -1,9 +1,13 @@
 package ong.bonanza.beneficiarioapi.adapter.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import ong.bonanza.beneficiarioapi.application.usecase.AutoCadastrarUsuarioUC;
 import ong.bonanza.beneficiarioapi.application.usecase.BuscarUsuariosPorEmailUC;
 
 @RequiredArgsConstructor
@@ -24,6 +29,8 @@ import ong.bonanza.beneficiarioapi.application.usecase.BuscarUsuariosPorEmailUC;
 public class UsuarioController {
 
     private final BuscarUsuariosPorEmailUC buscarUsuariosPorEmailUC;
+
+    private final AutoCadastrarUsuarioUC autoCadastrarUsuarioUC;
 
     @Operation(summary = "Buscar usuários", security = @SecurityRequirement(name = "bearerAuth"), description = "Busca usuários cadastrados no sistema")
     @ApiResponses(value = {
@@ -37,6 +44,26 @@ public class UsuarioController {
 
         return ResponseEntity
                 .ok(buscarUsuariosPorEmailUC.executar(new BuscarUsuariosPorEmailUC.ParametrosBuscaDTO(email, max)));
+
+    }
+
+    @Operation(summary = "Cadastrar-se como usuário na Base de Dados", security = @SecurityRequirement(name = "bearerAuth"), description = "Se cadastra na base através de seu token de acesso")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", content = @Content(schema = @Schema(implementation = Void.class))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "409", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "422", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = String.class)))
+    })
+    @PostMapping
+    public ResponseEntity<Void> cadastrarse(
+            @RequestBody String nome) {
+
+        final UUID usuarioId = autoCadastrarUsuarioUC.executar();
+
+        return ResponseEntity
+                .created(URI.create(String.format("usuarios/informacoes", usuarioId.toString())))
+                .build();
 
     }
 
