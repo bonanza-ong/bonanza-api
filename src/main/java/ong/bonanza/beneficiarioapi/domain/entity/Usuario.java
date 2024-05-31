cffc6d0b58e69eb110df7e28a73f4ae30e6a83b3
jhonatapers <jhonatapers@gmail.com>
2024-05-31 10:58:10 -0300

    [feat] adiciona anotations para persistencia

--- src/main/java/ong/bonanza/beneficiarioapi/domain/entity/Usuario.java
+++ src/main/java/ong/bonanza/beneficiarioapi/domain/entity/Usuario.java
@@ -2,6 +2,10 @@ package ong.bonanza.beneficiarioapi.domain.entity;
 
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,14 +13,20 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
+    @Id
     private UUID id;
 
+    @Transient
     private String email;
 
+    @Transient
     private String emailVerificado;
 
+    @Transient
     private Boolean ativo;
 
 }
