c29b519a55f4a4ee0a6894b8fe336b4189b538aa
jhonatapers <jhonatapers@gmail.com>
2024-05-31 11:01:30 -0300

    [feat] implementa findByUsuario

--- src/main/java/ong/bonanza/beneficiarioapi/domain/repository/PessoaRepository.java
+++ src/main/java/ong/bonanza/beneficiarioapi/domain/repository/PessoaRepository.java
@@ -1,11 +1,15 @@
 package ong.bonanza.beneficiarioapi.domain.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ong.bonanza.beneficiarioapi.domain.entity.Pessoa;
+import ong.bonanza.beneficiarioapi.domain.entity.Usuario;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, UUID> {
+
+    Optional<Pessoa> findByUsuario(Usuario usuario);
 
 }
