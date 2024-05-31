b97c845fd38d7009d5525b9be2f892d0ae7b6d5b
jhonatapers <jhonatapers@gmail.com>
2024-05-31 11:00:00 -0300

    [feat] implementa UsuarioRepository

--- src/main/java/ong/bonanza/beneficiarioapi/domain/repository/UsuarioRepository.java
+++ src/main/java/ong/bonanza/beneficiarioapi/domain/repository/UsuarioRepository.java
@@ -1,18 +1,13 @@
 package ong.bonanza.beneficiarioapi.domain.repository;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ong.bonanza.beneficiarioapi.domain.entity.Usuario;
 
 @Repository
-public interface UsuarioRepository {
-
-    Optional<Usuario> findById(UUID id);
-
-    List<Usuario> findByEmail(String email, Integer max);
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
 }
